dc45d6a4fed2c4229dcb11447331f706955cd04c
Enrico Ruggieri <enr.ruggieri.mail@gmail.com>
2022-01-05 03:55:00 +0100

    NotionClientInitializer.java implemented

---
+++ src/main/java/NotionClientInitializer.java
@@ -0,0 +1,29 @@
+import notion.api.v1.NotionClient;
+import notion.api.v1.exception.NotionAPIError;
+
+class NotionClientInitializer {
+
+    private NotionClient client;
+    private static final String NOTION_TOKEN = System.getenv("NOTION_TOKEN");
+
+    public NotionClientInitializer() {
+        if (NOTION_TOKEN == null)
+            throw new NullPointerException("Notion token env variables is empty!");
+        this.client = new NotionClient(NOTION_TOKEN);
+        testingConnection();
+    }
+
+    private void testingConnection() {
+        try {
+            // dummy request
+            client.listUsers().getResults();
+        } catch (NotionAPIError e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public NotionClient getClient() {
+        return client;
+    }
+}
